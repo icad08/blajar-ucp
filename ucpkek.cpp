3bc38893d11e2389d11484aea4ad1927f8bea026
icad08 <irsyadrm08@gmail.com>
2024-03-20 22:11:45 +0700

    hai

--- ucpkek.cpp
+++ ucpkek.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int arr[20]; 
+int n; 
+
+void sortir(){
+    int pass = 1;
+    do{
+        for(int i = 0; i <= n - 1 - pass; i++){
+            if (arr[i] > arr [i + 1])
+            {
+             int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+
+        }     
+        pass = pass + 1;
+        while (pass <= n- 1);
+    
+        while()
+    
+}
+
 int main()
 {
-    cout << "hai kamu" << endl;
     
 }
